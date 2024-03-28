d3a0067d0414b0da82c29b8b0dea1aa8b42597c2
nozomi-koborinai <kobofender@gmail.com>
2024-03-28 16:49:07 +0900

    feat: add future provider

--- lib/application/shop/states/shop_items.dart
+++ lib/application/shop/states/shop_items.dart
@@ -1,8 +1,24 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retail_dx/domain/shop_info.dart';
 import 'package:retail_dx/domain/shop_item.dart';
 
+import '../../../domain/repository/shop_info_repository.dart';
 import '../../../domain/repository/shop_item_repository.dart';
 
 final shopItemsProvider = FutureProvider<List<ShopItem>>(
   (ref) => ref.watch(shopItemRepositoryProvider).fetchAll(),
 );
+
+final shopItemFusionsProvider =
+    FutureProvider.family<List<(ShopInfo, ShopItem)>, String>(
+        (ref, itemId) async {
+  final result = <(ShopInfo, ShopItem)>[];
+  final shopItems =
+      await ref.watch(shopItemRepositoryProvider).fetchById(itemId);
+  for (final shopItem in shopItems) {
+    final shopInfo =
+        await ref.watch(shopInfoRepositoryProvider).fetchById(shopItem.shopId);
+    result.add((shopInfo, shopItem));
+  }
+  return result;
+});
