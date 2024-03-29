295d4ce609ee3d38326a2c031e4b1044d895935c
nozomi-koborinai <kobofender@gmail.com>
2024-03-29 19:34:39 +0900

    chore: add dummy dialog

--- lib/presentation/google_maps/maps_view_dx_page.dart
+++ lib/presentation/google_maps/maps_view_dx_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:retail_dx/application/shop/states/shop_items.dart';
@@ -138,13 +139,14 @@ class ItemDetailPage extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return ItemMapPage(item: item);
-                    },
-                  ),
-                );
+                // Navigator.of(context).push(
+                //   MaterialPageRoute(
+                //     builder: (context) {
+                //       // todo: デモでは使わない
+                //       return const ItemMapPage(items: []);
+                //     },
+                //   ),
+                // );
               },
               child: const Text('取り扱い店舗'),
             )
@@ -154,6 +156,8 @@ class ItemDetailPage extends StatelessWidget {
     );
   }
 }
+
+final isShowListStateProvider = StateProvider<bool>((_) => false);
 
 class ItemMapPage extends ConsumerStatefulWidget {
   const ItemMapPage({super.key, required this.item});
@@ -169,6 +173,10 @@ class _ItemMapPageState extends ConsumerState<ItemMapPage>
 
   @override
   Widget build(BuildContext context) {
+    final shopItemFusionsAsyncValue = ref.watch(
+      shopItemFusionsProvider(widget.item.id),
+    );
+
     ref.listen(currentMapPositionProvider, (_, next) async {
       final controller = await mapController.future;
       await controller.animateCamera(
@@ -181,72 +189,239 @@ class _ItemMapPageState extends ConsumerState<ItemMapPage>
       );
     });
 
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      body: _createGoogleMaps(),
+    return AsyncValueHandler(
+      value: shopItemFusionsAsyncValue,
+      builder: (shopItemFusions) {
+        return Stack(
+          children: [
+            Scaffold(
+              extendBodyBehindAppBar: true,
+              body: GoogleMap(
+                markers: shopItemFusions.map(
+                  (shopItemFusion) {
+                    final shopInfo = shopItemFusion.$1;
+                    final shopItem = shopItemFusion.$2;
+                    return Marker(
+                      markerId: MarkerId(shopInfo.id),
+                      position: LatLng(
+                        shopInfo.location.latitude,
+                        shopInfo.location.longitude,
+                      ),
+                      infoWindow: InfoWindow(
+                        title: '在庫： ${shopItem.quantity.toString()}',
+                      ),
+                    );
+                  },
+                ).toSet(),
+                // polylines: shopItemFusions.map(
+                //   (shopItemFusion) {
+                //     final shopInfo = shopItemFusion.$1;
+                //     return Polyline(
+                //       polylineId: PolylineId(shopInfo.id),
+                //       color: Colors.orange,
+                //       width: 4,
+                //       points: [
+                //         ref.read(currentMapPositionProvider),
+                //         LatLng(
+                //           shopInfo.location.latitude,
+                //           shopInfo.location.longitude,
+                //         ),
+                //       ],
+                //     );
+                //   },
+                // ).toSet(),
+                minMaxZoomPreference: const MinMaxZoomPreference(0, 20),
+                myLocationEnabled: true,
+                zoomControlsEnabled: true,
+                onMapCreated: (controller) async {
+                  mapController.complete(controller);
+                  final value = await rootBundle
+                      .loadString('assets/maps/maps_style.json');
+                  final futureController = await mapController.future;
+                  await futureController.setMapStyle(value);
+                },
+                myLocationButtonEnabled: true,
+                initialCameraPosition: CameraPosition(
+                  target: ref.read(currentMapPositionProvider),
+                  zoom: 13.0,
+                ),
+              ),
+            ),
+            ref.watch(isShowListStateProvider)
+                ? Dialog(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              children: [
+                                Image.network(
+                                  widget.item
+                                      .imageUrl, // Replace with your item's image URL
+                                  fit: BoxFit.cover,
+                                  width: double.infinity,
+                                  height: 120,
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  'Pickup today',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                SizedBox(height: 8),
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                    hintText: 'Enter your postcode',
+                                    suffixIcon: Icon(Icons.search),
+                                    border: OutlineInputBorder(),
+                                    contentPadding:
+                                        EdgeInsets.symmetric(horizontal: 8),
+                                  ),
+                                ),
+                                SizedBox(height: 16),
+                              ],
+                            ),
+                          ),
+                          Divider(),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: shopItemFusions
+                                .length, // Replace with your actual store list count
+                            itemBuilder: (BuildContext context, int index) {
+                              // Replace with your actual data model
+                              final shop = shopItemFusions[index];
+                              return ListTile(
+                                title: Text(shop.$1.name),
+                                // subtitle: Text(
+                                //   '${shop.distance} mi - ${shop.time} min',
+                                // ),
+                                trailing: ElevatedButton(
+                                  onPressed: () {
+                                    // Handle your curbside pickup logic here
+                                  },
+                                  child: Text('Curbside pickup'),
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Theme.of(context).primaryColor,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                : Positioned(
+                    top: 35,
+                    bottom: 0,
+                    left: 35,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 200,
+                          child: Image.network(
+                            widget.item.imageUrl,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.item.name,
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 16),
+                              ElevatedButton(
+                                onPressed: () => ref
+                                    .read(isShowListStateProvider.notifier)
+                                    .update((_) => true),
+                                child: Text('Show list'),
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.blue, // Button color
+                                  onPrimary: Colors.white, // Text color
+                                ),
+                              ),
+                              // Here you can add more UI elements as needed
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+          ],
+        );
+      },
+      loading: () => const OverlayLoading(),
+      error: (p0, p1) => Text(p0.toString()),
     );
   }
 
-  // Widget _createWideScreenBody() {
-  //   return Stack(
-  //     children: [
-  //       _createGoogleMaps(isWideScreen: true),
-  //       Positioned(
-  //         top: 0,
-  //         bottom: 0,
-  //         left: 0,
-  //         width: 800,
-  //         child: Column(
-  //           children: [
-  //             _createSidePanel(),
-  //             const SideDetailPanel(),
-  //           ],
-  //         ),
-  //       ),
-  //     ],
-  //   );
-  // }
-
-  Widget _createGoogleMaps() {
-    return GoogleMap(
-      // markers: {
-      //   Marker(
-      //     infoWindow: InfoWindow(
-      //       title: ref.watch(selectedPlaceProvider)?.name,
-      //     ),
-      //     markerId: const MarkerId('selectedPlace'),
-      //     position: LatLng(
-      //       selectedPlace?.latitude ?? -1,
-      //       selectedPlace?.longitude ?? -1,
-      //     ),
-      //     icon: BitmapDescriptor.defaultMarkerWithHue(
-      //       BitmapDescriptor.hueAzure,
-      //     ),
-      //   ),
-      // },
-      // polylines: {
-      //   Polyline(
-      //     polylineId: const PolylineId('route'),
-      //     color: Colors.pinkAccent,
-      //     width: 4,
-      //     points: ref.watch(_routesProvider),
-      //   ),
-      // },
-      minMaxZoomPreference: const MinMaxZoomPreference(0, 20),
-      myLocationEnabled: true,
-      zoomControlsEnabled: true,
-      onMapCreated: (controller) async {
-        mapController.complete(controller);
-        // final value =
-        //     await rootBundle.loadString('assets/maps/maps_style.json');
-        // final futureController = await mapController.future;
-        // await futureController.setMapStyle(value);
+  void _showPickupDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          elevation: 16,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.network(
+                'アイテムの画像URL', // アイテムの画像URLを指定
+                height: 120,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Text(
+                      'Pickup today',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    // ここに店舗リストを表示するためのウィジェットを作成してください。
+                    // 例えば、ListView.builderを使用してリストを作成します。
+                    // ListView.builder(
+                    //   shrinkWrap: true,
+                    //   itemCount: storeList.length, // 店舗情報のリストの長さ
+                    //   itemBuilder: (context, index) {
+                    //     final store = storeList[index];
+                    //     return ListTile(
+                    //       title: Text(store.name),
+                    //       subtitle: Text('${store.distance} km - ${store.time} min'),
+                    //       trailing: ElevatedButton(
+                    //         onPressed: () {
+                    //           // ピックアップ処理など
+                    //         },
+                    //         child: Text('Curbside pickup'),
+                    //       ),
+                    //     );
+                    //   },
+                    // ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
       },
-      myLocationButtonEnabled: true,
-      initialCameraPosition: CameraPosition(
-        target: ref.read(currentMapPositionProvider),
-        zoom: 13.0,
-      ),
     );
   }
 }
