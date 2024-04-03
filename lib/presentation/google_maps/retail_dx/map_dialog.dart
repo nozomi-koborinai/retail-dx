24deb05e778a8d0c80254fe8ae20b6e4064195c9
nozomi-koborinai <kobofender@gmail.com>
2024-04-03 18:03:47 +0900

    chore: Dialog のサイズを小さくした

--- lib/presentation/google_maps/retail_dx/map_dialog.dart
+++ lib/presentation/google_maps/retail_dx/map_dialog.dart
@@ -1,3 +1,4 @@
+// MapDialog.dart
 import 'package:flutter/material.dart';
 import 'package:pointer_interceptor/pointer_interceptor.dart';
 
@@ -12,70 +13,69 @@ class MapDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-      surfaceTintColor: Colors.white,
-      child: PointerInterceptor(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    Image.network(
-                      imageUrl,
-                      fit: BoxFit.cover,
-                      width: double.infinity,
-                      height: 120,
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Pickup today',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
+    return Center(
+      child: Container(
+        color: Colors.white,
+        width: MediaQuery.of(context).size.width * 0.4,
+        padding: const EdgeInsets.all(8.0),
+        child: PointerInterceptor(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Image.network(
+                        imageUrl,
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                        height: 120,
                       ),
-                    ),
-                    SizedBox(height: 8),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: 'Enter your postcode',
-                        suffixIcon: Icon(Icons.search),
-                        border: OutlineInputBorder(),
-                        contentPadding: EdgeInsets.symmetric(horizontal: 8),
+                      SizedBox(height: 8),
+                      Text(
+                        'Pickup today',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 16),
-                  ],
+                      SizedBox(height: 8),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Enter your postcode',
+                          suffixIcon: Icon(Icons.search),
+                          border: OutlineInputBorder(),
+                          contentPadding: EdgeInsets.symmetric(horizontal: 8),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                    ],
+                  ),
                 ),
-              ),
-              Divider(),
-              ListView.builder(
-                shrinkWrap: true,
-                itemCount: shopItemFusions
-                    .length, // Replace with your actual store list count
-                itemBuilder: (BuildContext context, int index) {
-                  // Replace with your actual data model
-                  final shop = shopItemFusions[index];
-                  return ListTile(
-                    title: Text(shop.$1.name),
-                    // subtitle: Text(
-                    //   '${shop.distance} mi - ${shop.time} min',
-                    // ),
-                    trailing: ElevatedButton(
-                      onPressed: () {
-                        // Handle your curbside pickup logic here
-                      },
-                      child: Text('Curbside pickup'),
-                      style: ElevatedButton.styleFrom(
-                        primary: Theme.of(context).primaryColor,
+                Divider(),
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: shopItemFusions.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final shop = shopItemFusions[index];
+                    return ListTile(
+                      title: Text(shop.$1.name),
+                      trailing: ElevatedButton(
+                        onPressed: () {
+                          // Handle your curbside pickup logic here
+                        },
+                        child: Text('Curbside pickup'),
+                        style: ElevatedButton.styleFrom(
+                          primary: Theme.of(context).primaryColor,
+                        ),
                       ),
-                    ),
-                  );
-                },
-              ),
-            ],
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
