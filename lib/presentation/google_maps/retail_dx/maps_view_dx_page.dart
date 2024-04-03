51b9d5d821abbca09bcdc44abfa87775de2002c4
nozomi-koborinai <kobofender@gmail.com>
2024-04-03 17:12:34 +0900

    refactor: maps view dx page

--- lib/presentation/google_maps/retail_dx/maps_view_dx_page.dart
+++ lib/presentation/google_maps/retail_dx/maps_view_dx_page.dart
@@ -10,153 +10,8 @@ import 'package:retail_dx/domain/shop_item.dart';
 import 'package:retail_dx/presentation/components/async_value_handler.dart';
 import 'package:retail_dx/presentation/components/loading.dart';
 
-import '../../application/google_maps/states/current_map_position.dart';
-import '../mixin/error_handler_mixin.dart';
-
-class ItemListPage extends ConsumerWidget {
-  const ItemListPage({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final itemsAsyncValue = ref.watch(shopItemsProvider);
-    return AsyncValueHandler(
-      value: itemsAsyncValue,
-      builder: (items) {
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Item List'),
-            actions: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.search),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: const Icon(Icons.shopping_cart),
-                onPressed: () {},
-              )
-            ],
-          ),
-          body: GridView.builder(
-            padding: const EdgeInsets.all(16),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 16,
-              mainAxisSpacing: 16,
-              childAspectRatio: 0.8,
-            ),
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              return _ProductCard(item: items[index]);
-            },
-          ),
-        );
-      },
-      loading: () => const OverlayLoading(),
-      error: (p0, p1) => Center(
-        child: Text('error: ${p0.toString()}'),
-      ),
-    );
-  }
-}
-
-class _ProductCard extends StatelessWidget {
-  const _ProductCard({
-    required this.item,
-  });
-  final ShopItem item;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 4.0,
-      clipBehavior: Clip.antiAlias,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: InkWell(
-        onTap: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) {
-                return ItemDetailPage(item: item);
-              },
-            ),
-          );
-        },
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage('https://picsum.photos/200/300'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Text(
-                item.name,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-                '¥${item.price}',
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: Colors.red,
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ItemDetailPage extends StatelessWidget {
-  const ItemDetailPage({super.key, required this.item});
-  final ShopItem item;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Item Detail'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                // Navigator.of(context).push(
-                //   MaterialPageRoute(
-                //     builder: (context) {
-                //       // todo: デモでは使わない
-                //       return const ItemMapPage(items: []);
-                //     },
-                //   ),
-                // );
-              },
-              child: const Text('取り扱い店舗'),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
+import '../../../application/google_maps/states/current_map_position.dart';
+import '../../mixin/error_handler_mixin.dart';
 
 final isShowListStateProvider = StateProvider<bool>((_) => false);
 
@@ -253,6 +108,7 @@ class _ItemMapPageState extends ConsumerState<ItemMapPage>
             ),
             ref.watch(isShowListStateProvider)
                 ? Dialog(
+                    surfaceTintColor: Colors.white,
                     child: PointerInterceptor(
                       child: SingleChildScrollView(
                         child: Column(
