d0b0fd778c1a756f72970064138521458572193f
nozomi-koborinai <kobofender@gmail.com>
2024-04-04 18:04:56 +0900

    chore: mapdialog

--- lib/presentation/google_maps/retail_dx/map_dialog.dart
+++ lib/presentation/google_maps/retail_dx/map_dialog.dart
@@ -1,89 +1,88 @@
 // MapDialog.dart
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pointer_interceptor/pointer_interceptor.dart';
+import 'package:retail_dx/presentation/google_maps/retail_dx/store_list_tile.dart';
 
+import '../../../application/google_maps/states/current_map_position.dart';
 import '../../../domain/shop_info.dart';
 import '../../../domain/shop_item.dart';
 
-class MapDialog extends StatelessWidget {
+class MapDialog extends ConsumerWidget {
   const MapDialog(
       {super.key, required this.imageUrl, required this.shopItemFusions});
   final String imageUrl;
   final List<(ShopInfo, ShopItem)> shopItemFusions;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Center(
       child: Container(
         decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.circular(16.0), // 角の丸み
+          borderRadius: BorderRadius.circular(16.0),
           boxShadow: [
             BoxShadow(
               color: Colors.grey.withOpacity(0.5),
               spreadRadius: 5,
               blurRadius: 7,
-              offset: Offset(0, 3), // 影の位置を調整
+              offset: const Offset(0, 3),
             ),
           ],
         ),
-        width: MediaQuery.of(context).size.width * 0.4,
-        padding: const EdgeInsets.all(8.0),
+        width: MediaQuery.of(context).size.width * 0.5,
         child: PointerInterceptor(
           child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
+            child: Row(
               children: [
-                Padding(
+                Container(
+                  width: 120.0,
+                  height: 120.0,
                   padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      Image.network(
-                        imageUrl,
-                        fit: BoxFit.cover,
-                        width: double.infinity,
-                        height: 120,
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Pickup today',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          hintText: 'Enter your postcode',
-                          suffixIcon: Icon(Icons.search),
-                          border: OutlineInputBorder(),
-                          contentPadding: EdgeInsets.symmetric(horizontal: 8),
-                        ),
-                      ),
-                      SizedBox(height: 16),
-                    ],
+                  child: Image.network(
+                    imageUrl,
+                    fit: BoxFit.cover,
                   ),
                 ),
-                Divider(),
-                ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: shopItemFusions.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final shop = shopItemFusions[index];
-                    return ListTile(
-                      title: Text(shop.$1.name),
-                      trailing: ElevatedButton(
-                        onPressed: () {
-                          // Handle your curbside pickup logic here
-                        },
-                        child: Text('Curbside pickup'),
-                        style: ElevatedButton.styleFrom(
-                          primary: Theme.of(context).primaryColor,
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // const Text(
+                        //   'Pickup today',
+                        //   style: TextStyle(
+                        //     fontWeight: FontWeight.bold,
+                        //     fontSize: 20,
+                        //   ),
+                        // ),
+                        // TextFormField(
+                        //   decoration: const InputDecoration(
+                        //     hintText: 'Enter your postcode',
+                        //     suffixIcon: Icon(Icons.search),
+                        //     border: OutlineInputBorder(),
+                        //     contentPadding: EdgeInsets.symmetric(horizontal: 8),
+                        //   ),
+                        // ),
+                        ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: shopItemFusions.length,
+                          itemBuilder: (context, index) {
+                            final shopItemFusion = shopItemFusions[index];
+                            if (shopItemFusion.$2.quantity == 0)
+                              return const SizedBox();
+                            return StoreListTile(
+                              shopItemFusion: shopItemFusion,
+                              currentLocation: ref.read(
+                                currentMapPositionProvider,
+                              ),
+                            );
+                          },
                         ),
-                      ),
-                    );
-                  },
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
